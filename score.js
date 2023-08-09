3d3fee3a9cc0faa50a48704a3427de672b3df746
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-08-09 15:30:26 +0300

    Update score.js

--- score.js
+++ score.js
@@ -1,9 +1,1 @@
-let seconds = 0;
-let secondsCnt = document.getElementById("seconds-counter");
 
-function incrementSeconds() {
-    ++seconds;
-    secondsCnt.innerText = seconds;
-}
-
-let cancel = setInterval(incrementSeconds, 1000);
