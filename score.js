5b88e5f86170c3bf6319201cc619920fc15cd1ae
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-08-09 15:45:38 +0300

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,9 @@
+let seconds = 0;
+let secondsCnt = document.getElementById("seconds-counter");
 
+let secondsInterval = setInterval(function() {
+    if (obstacle1.style.top == "600px" || obstacle2.style.top == "600px") {
+        ++seconds;
+    }
+    secondsCnt.innerText = seconds;
+}, 0.1);
